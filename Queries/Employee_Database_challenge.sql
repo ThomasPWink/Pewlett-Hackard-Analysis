357fe3026545567f88de1f289b61d15242c7df2f
ThomasPWink <thomas.p.wink@gmail.com>
2022-12-16 23:47:44 -0700

    updated challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -52,3 +52,95 @@ INNER JOIN titles as t
 WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no asc, t.to_date DESC;
+
+-- Analysis queries 
+Select title, count(title)
+from unique_titles
+group by title
+order by count(title) desc;
+
+-- creating a current total employees table 
+
+SELECT distinct on (emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	t.title,  
+	t.to_date
+INTO all_current_emp
+FROM employees as e
+INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (t.to_date = '9999-01-01')
+order by emp_no asc, to_date desc; 
+
+select count(emp_no) 
+from all_current_emp;
+select sum(retiring_total)
+from retiring_titles;
+select count(emp_no)
+from employees;
+
+
+select title, count(title)
+into mentorship_eligibility_total
+from mentorship_eligibilty
+group by title
+order by count(title) desc;
+
+select title, count(title)
+into employee_title_total
+from all_current_emp
+group by title
+order by count(title) desc;
+
+-- alter tables
+
+alter table employee_title_total
+rename column count TO employee_total;
+
+-- creating totals graph
+select e.title,
+	e.employee_total,
+	rt.retiring_total,
+	me.mentorship_total
+into overall_totals
+from employee_title_total as e
+inner join retiring_titles as rt
+on (e.title = rt.title)
+inner join mentorship_eligibility_total as me
+on (rt.title = me.title)
+order by rt.retiring_total desc;
+
+-- totals 
+select count(emp_no)
+from all_current_emp;
+
+select count(emp_no)
+from unique_titles;
+
+select sum(employee_total)
+from employemployee_title_total;
+
+-- Delete tables
+--drop table overall_totals;
+
+--  duplicates 
+select emp_no, count(emp_no)
+from unique_titles
+group by emp_no
+having count(emp_no) > 1;
+
+-- graphs 
+select * from retiring_titles;
+
+select sum(mentorship_total) from mentorship_eligibility_total;
+
+select * from overall_totals;
+
+select * from mentorship_eligibilty;
+
+alter table overall_totals
+add percent_retiring int;
+
+alter table overall_totals
+add percent_mentorship AS (mentorship_total / retiring_total);
